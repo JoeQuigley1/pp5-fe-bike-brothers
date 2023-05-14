942f3028d35ce48ed7f0b1d1384afa9c47d5647e
JoeQuigley1 <joeyjtq@hotmail.com>
2023-05-14 18:46:53 +0000

    Add meetupPage and link to meetup with skeleton styling

---
+++ src/pages/meetups/Meetup.js
@@ -0,0 +1,53 @@
+import React from "react";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import { Card, Media } from "react-bootstrap";
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
+import Avatar from "../../components/Avatar";
+
+const Meetup = (props) => {
+  const {
+    id,
+    owner,
+    profile_id,
+    profile_image,
+    title,
+    content,
+    updated_at,
+    date,
+    time,
+    city,
+    venue,
+    bike_type,
+    spaces,
+    duration,
+    meetupPage
+  } = props;
+
+  const currentUser = useCurrentUser();
+  const is_owner = currentUser?.username === owner;
+
+  
+  return <Card>
+    <Card.Body>
+        <Media className="align-items-center justify-content-between">
+            <Link to={`/profiles/${profile_id}`}>
+            <Avatar src={profile_image} height={30} />
+             {owner}
+            </Link>
+            <div><span>{updated_at}</span>
+            {is_owner && meetupPage && "..." } </div>
+        </Media>
+    </Card.Body>
+    <h2> Title: {title}</h2>
+    <p>Date:{date}</p>
+    <p> {content}</p>
+    <p>What city: {city}</p>
+    <p>Location: {venue}</p>
+    <p>Time: {time}</p>
+    <p>Duration: {duration}</p>
+    <p>What bikes: {bike_type}</p>
+    <p>Capacity: {spaces}</p>
+  </Card>;
+};
+
+export default Meetup;
--- src/pages/meetups/MeetupPage.js
+++ src/pages/meetups/MeetupPage.js
@@ -7,6 +7,7 @@ import Container from "react-bootstrap/Container";
 import appStyles from "../../App.module.css";
 import { axiosReq } from "../../api/axiosDefaults";
 import { useParams } from "react-router-dom/cjs/react-router-dom.min";
+import Meetup from "./Meetup";
 
 function MeetupPage() {
     const { id } = useParams();
@@ -34,13 +35,7 @@ function MeetupPage() {
     <Row className="h-100">
       <Col className="py-2 p-0 p-lg-2" lg={8}>
         <p>Popular profiles for mobile</p>
-        <p>Post component</p>
-        <Container className={appStyles.Content}>
-          Comments
-        </Container>
-      </Col>
-      <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
-        Popular profiles for desktop
+        <Meetup {...meetup.results[0]} setMeetup={setMeetup} meetupPage />
       </Col>
     </Row>
   );
