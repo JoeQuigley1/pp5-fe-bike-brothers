d5f9638295899ecc0ef5069fa3ec17f9f80368d7
JoeQuigley1 <joeyjtq@hotmail.com>
2023-05-23 20:59:56 +0000

    connect contact to backend API

--- src/pages/contact/CreateContactForm.js
+++ src/pages/contact/CreateContactForm.js
@@ -1,21 +1,95 @@
-import React, { useState } from 'react'
+
+import React, { useState } from "react";
+import { Button, Container, Form, Row } from "react-bootstrap";
+import { useHistory } from "react-router";
+import { axiosReq } from "../../api/axiosDefaults";
 
 function CreateContactForm() {
+  const [errors, setErrors] = useState({});
 
-    const [contactFormData, setContactFromData] = useState({
-        fname: "",
-        lname: "",
-        email: "",
-        content: "",
-        create_at: "",
+  const [contactFormData, setContactFromData] = useState({
+    fname: "",
+    lname: "",
+    email: "",
+    content: "",
+    
+  });
+
+  const { fname, lname, email, content,  } = contactFormData;
+
+  const history = useHistory();
+
+  const handleChange = (event) => {
+    setContactFromData({
+      ...contactFormData,
+      [event.target.name]: event.target.value,
     });
+  };
 
-    const {
-        fname, lname, email, content, created_at,
-    } = contactFormData
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      await axiosReq.post("/contact/", contactFormData);
+      history.push("/about");
+    } catch (err) {
+      console.log(err);
+      setErrors(err.response?.data);
+    }
+  };
+
   return (
-    <div>CreateContactForm</div>
-  )
+    <Container>
+      <h2>Please get in touch!</h2>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group>
+          <Form.Control
+            type="text"
+            name="fname"
+            value={fname}
+            onChange={handleChange}
+            aria-label="first name"
+            placeholder="First Name"
+          ></Form.Control>
+        </Form.Group>
+        <Form.Group>
+          <Form.Control
+            type="text"
+            name="lname"
+            value={lname}
+            onChange={handleChange}
+            aria-label="last name"
+            placeholder="Last Name"
+          ></Form.Control>
+        </Form.Group>
+        <Form.Group>
+          <Form.Control
+            type="text"
+            name="email"
+            value={email}
+            onChange={handleChange}
+            aria-label="email"
+            placeholder="please write your email"
+          ></Form.Control>
+        </Form.Group>
+        <Form.Group>
+          <Form.Control
+            type="textarea"
+            name="content"
+            value={content}
+            onChange={handleChange}
+            aria-label="content"
+            placeholder="Please write your message here.."
+          ></Form.Control>
+        </Form.Group>
+        
+
+        <Button type="submit" >
+            Submit
+          </Button>
+      </Form>
+    
+    </Container>
+  );
 }
 
-export default CreateContactForm
+export default CreateContactForm;
