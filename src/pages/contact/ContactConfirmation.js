273784c5ca9103d75745a5a010aa12a94b48200a
JoeQuigley1 <joeyjtq@hotmail.com>
2023-05-25 20:40:12 +0000

    Add confirmation message and basic styling

--- src/pages/contact/ContactConfirmation.js
+++ src/pages/contact/ContactConfirmation.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import { Container } from 'react-bootstrap'
+import styles from "../../styles/Confirmation.module.css"
 
 const ContactConfirmation = () => {
   return (
-    <div>ContactConfirmation</div>
+    <Container className={styles.Content}>
+    <h2>Thank you for getting in touch!</h2>
+
+  <p>One of our Admins will get back to you on your query</p>
+    </Container>
+    
   )
 }
 
